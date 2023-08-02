3d466577c6c437cbe079dd9766c1f4ab403c630f
roshanraviraj89 <124732388+roshanraviraj89@users.noreply.github.com>
2023-08-02 18:05:18 +0100

    Update root_setup.sh

--- rac_ol8_21c/DNS/scripts/root_setup.sh
+++ rac_ol8_21c/DNS/scripts/root_setup.sh
@@ -7,10 +7,11 @@ sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 #sed -i '2inameserver 192.168.68.114' /etc/resolv.conf
-echo -e 'nameserver 192.168.68.114\n search localdomain' > /etc/resolv.conf
+echo -e 'nameserver 192.168.68.114\n search localdomain' > /etc/resolv.conf      ## dns server ip
+echo -e  'nameserver 8.8.8.8'  >> /etc/resolv.conf                               ##add google dns entry
+#echo -e 'nameserver 10.0.2.3\n search localdomain' >> /etc/resolv.conf
+systemctl restart named
 echo -e 'DNS1=192.168.68.114\nDOMAIN=localdomain' >> /etc/sysconfig/network-scripts/ifcfg-eth1
-echo -e 'DNS1=192.168.68.114\nDOMAIN=localdomain' >> /etc/sysconfig/network-scripts/ifcfg-eth2
-echo -e 'DNS1=192.168.68.114\nDOMAIN=localdomain' >> /etc/sysconfig/network-scripts/ifcfg-eth3
 sed -i -e "s|\[main\]|\[main\]\ndns=none|g" /etc/NetworkManager/NetworkManager.conf
 systemctl restart NetworkManager.service
 sh /vagrant/scripts/adduser.sh
